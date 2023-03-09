b8554e74986c4a001fd484f6e1a09b078c2183d0
Sirimas2828 <biwsiri.k@gmail.com>
2023-03-09 15:18:02 +0700

    try Challenge

---
+++ Script2.js
@@ -0,0 +1,5 @@
+function myFunction() {
+    alert("Hello World");
+    console.log("The button has been clicked");
+    document.getElementById("text").innerHTML = "The button has been clicked";
+}
